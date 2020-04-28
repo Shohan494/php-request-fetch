7ba7363c1e194b27c42bd781181c72508346bd80
Shohan494 <shohan12224943@gmail.com>
2020-04-29 02:35:08 +0600

    php get, post, cookie = request

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 <a href="phpinfo.php">serverVariables</a>
 <br>
 <a href="smallPhpInfo.php">smallPhpInfo</a>
+<br>
+<a href="testGetPostCookie.php">testGetPostCookie</a>
--- testGet.php
+++
@@ -1,8 +0,0 @@
-<pre>
-<?php
-print_r($_GET);
-?>
-</pre>
-
-
-// The GET variables are passed through urldecode(). 
